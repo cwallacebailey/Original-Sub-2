870026e286a058e0c7d58a796245d49123ca91c5
cwallacebailey <cwallacebailey@gmail.com>
2022-01-07 10:58:32 +0000

    update function beginGame() to show celebrity image and array of possible asnwers

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -13,10 +13,10 @@ const array = [
     img2 : 'assets/images/chris2.png',
     img3 : 'assets/images/chris3.png',
     img4 : 'assets/images/chris4.png',
-    choice1 : 'David Beckham',
-    choice2 : 'Jared Leto',
-    choice3 : 'Christian Bale',
-    choice4 : 'Hugh Jackman',
+    choice1 : '1. David Beckham',
+    choice2 : '2. Jared Leto',
+    choice3 : '3. Christian Bale',
+    choice4 : '4. Hugh Jackman',
     answer : 3,
     },
     {
@@ -25,10 +25,10 @@ const array = [
     img2 : 'assets/images/dan2.jpg',
     img3 : 'assets/images/dan3.jpg',
     img4 : 'assets/images/dan4.jpg',
-    choice1 : 'Dany trejo',
-    choice2 : 'Cheech Marin',
-    choice3 : 'Tom Savini',
-    choice4 : 'John P. Fedele',
+    choice1 : '1. Dany trejo',
+    choice2 : '2. Cheech Marin',
+    choice3 : '3. Tom Savini',
+    choice4 : '4. John P. Fedele',
     answer : 1,
     },
     {
@@ -37,10 +37,10 @@ const array = [
     img2 : 'assets/images/eli2.png',
     img3 : 'assets/images/eli3.png',
     img4 : 'assets/images/eli4.png',
-    choice1 : 'Robbie Coltrane',
-    choice2 : 'Daniel Radcliffe',
-    choice3 : 'Elija Wood',
-    choice4 : 'Oliver Phelps',
+    choice1 : '1. Robbie Coltrane',
+    choice2 : '2. Daniel Radcliffe',
+    choice3 : '3. Elija Wood',
+    choice4 : '4. Oliver Phelps',
     answer : 3,
     },
     {
@@ -49,10 +49,10 @@ const array = [
     img2 : 'assets/images/lea2.png',
     img3 : 'assets/images/lea3.png',
     img4 : 'assets/images/lea4.png',
-    choice1 : 'Rachel Weisz',
-    choice2 : 'Lea Seydoux',
-    choice3 : 'Ariana Labed',
-    choice4 : 'Saoirse Ronan',
+    choice1 : '1. Rachel Weisz',
+    choice2 : '2. Lea Seydoux',
+    choice3 : '3. Ariana Labed',
+    choice4 : '4. Saoirse Ronan',
     answer : 2,
     },
     {
@@ -61,10 +61,10 @@ const array = [
     img2 : 'assets/images/mah2.png',
     img3 : 'assets/images/mah3.png',
     img4 : 'assets/images/mah4.png',
-    choice1 : 'Wesley Snipes',
-    choice2 : 'Anthony Mackie',
-    choice3 : 'Don Cheadle',
-    choice4 : 'Mahershala Ali.',
+    choice1 : '1. Wesley Snipes',
+    choice2 : '2. Anthony Mackie',
+    choice3 : '3. Don Cheadle',
+    choice4 : '4. Mahershala Ali.',
     answer : 3,
     },
     {
@@ -73,10 +73,10 @@ const array = [
     img2 : 'assets/images/nic2.png',
     img3 : 'assets/images/nic3.png',
     img4 : 'assets/images/nic4.png',
-    choice1 : 'David Beckham',
-    choice2 : 'Thomas Jane',
-    choice3 : 'John Travolta',
-    choice4 : 'Nicholas Cage',
+    choice1 : '1. David Beckham',
+    choice2 : '2. Thomas Jane',
+    choice3 : '3. John Travolta',
+    choice4 : '4. Nicholas Cage',
     answer : 4,
     },
     {
@@ -85,10 +85,10 @@ const array = [
     img2 : 'assets/images/ser2.png',
     img3 : 'assets/images/ser3.png',
     img4 : 'assets/images/ser4.png',
-    choice1 : 'Serena Williams',
-    choice2 : 'Isha Price',
-    choice3 : 'Venus Williams',
-    choice4 : 'Lyndrea Price',
+    choice1 : '1. Serena Williams',
+    choice2 : '2. Isha Price',
+    choice3 : '3. Venus Williams',
+    choice4 : '4. Lyndrea Price',
     answer : 1,
     },
 ]
@@ -98,13 +98,20 @@ function beginGame() {
     let picture2 = document.getElementById("pic2")
     let picture3 = document.getElementById("pic3")
     let picture4 = document.getElementById("pic4")
+    let q1 = document.getElementById("q1")
+    let q2 = document.getElementById("q2")
+    let q3 = document.getElementById("q3")
+    let q4 = document.getElementById("q4")
     let start = Math.floor(Math.random()*6)
     
     picture1.src = array[start].img1;
     picture2.src = array[start].img2;
     picture3.src = array[start].img3;
     picture4.src = array[start].img4;
-
+    q1.innerHTML = array[start].choice1;
+    q2.innerHTML = array[start].choice2;
+    q3.innerHTML = array[start].choice3;
+    q4.innerHTML = array[start].choice4;
 }
 
 
